aea108e81498e253d2ea5f3b89f89d230a3609d3
kyhyun <k29176@gmail.com>
2022-08-25 22:25:04 +0900

    feat : Add update card and delete card functionality

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: 1,
       name: 'yononi',
       company: 'white-hand',
@@ -19,7 +19,7 @@ const Maker = ({ authService }) => {
       fileName: 'yononi',
       fileUrl: null,
     },
-    {
+    2: {
       id: 2,
       name: 'dauny',
       company: 'jong-ken-dang',
@@ -30,7 +30,7 @@ const Maker = ({ authService }) => {
       fileName: 'dauny',
       fileUrl: null,
     },
-    {
+    3: {
       id: 3,
       name: 'chocho',
       company: 'myHome',
@@ -41,7 +41,7 @@ const Maker = ({ authService }) => {
       fileName: 'dog',
       fileUrl: null,
     },
-  ]);
+  });
 
   const navigate = useNavigate();
   const onLogout = () => {
@@ -56,16 +56,27 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const update = [...cards, card];
-    setCards(update);
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
